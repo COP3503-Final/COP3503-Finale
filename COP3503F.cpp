3d61b3666956a4808e88a97d619f0699793e8d53
Kevin Asistores <kasistores@ufl.edu>
2016-04-16 19:41:41 -0400

    deleting non cpp file

---
+++ COP3503F.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  cout << "=====================================================================\n";
+  cout << "Welcome to the Computer Vision Recognition System Acquisition Mission\n\n";
+  cout << "Login\n";
+  cout << "Add User\n";
+  cout << "Exit\n";
+  cout << "=====================================================================\n";
+
+
+  return 0
+}
